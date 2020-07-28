29f7ebde718801ef7064ccd14ab4146918674007
zhaoyaping1901 <1193331901@qq.com>
2020-07-28 22:31:00 +0800

    '轮播插件'

---
+++ src/components/common/swiper/MySwiper.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="mySwiper">
+		<swiper ref="mySwiper" :options="swiperOptions">
+      <swiper-slide v-for="(item,index) in swipers">
+        <img style="width:100%;display:block;height:230px" :src="item.goodsImg">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+	</div>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import '@/assets/css/swiper.css'
+export default {
+  name: 'mySwiper',
+  components: {
+		Swiper,
+		SwiperSlide
+  },
+  props:{
+		swipers:{
+			type:Array,
+			value:[]
+		},
+		swiperOptions:{
+			type:Object,
+			value:{}
+		}
+  },
+  data(){
+  	return {
+  		
+  	}
+  },
+  computed: {
+    // swiper() {
+    //   return this.$refs.mySwiper.$swiper
+    // }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,57 +3,53 @@
   	<nav-bar class="home-nav">
   		<div slot="_center">购物街</div>
   	</nav-bar>
-    <swiper ref="mySwiper" :options="swiperOptions">
-      <swiper-slide v-for="(item,index) in swipers">
-        <img style="width:100%;display:block;height:230px" :src="item.goodsImg">
-      </swiper-slide>
-      <div class="swiper-pagination" slot="pagination"></div>
-    </swiper>
+    <my-swiper :swipers="swipers" :swiperOptions="swiperOptions"></my-swiper>
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/common/navbar/NavBar'
 import { getHomeMultation } from '@/network/home'
+import MySwiper from '@/components/common/swiper/MySwiper'
 
-import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
-import '@/assets/css/swiper.css'
 export default {
   name: 'Home',
   components: {
-    NavBar,
-    Swiper,
-    SwiperSlide
+    NavBar,MySwiper
   },
-  created(){
-    getHomeMultation().then(res => {
-      console.log(res)
-      this.swipers = res;
-    }).catch(err => {
-      console.log(err)
-    })
-  },
-  mounted () {
-      console.log('Current Swiper instance object', this.swiper)
-      //this.swiper.slideTo(3, 1000, true)
-  },
-  computed: {
-      swiper() {
-        return this.$refs.mySwiper.$swiper
-      }
-    },
   data () {
     return {
       swipers:[],
-        swiperOptions: {
-          loop:true,
-          autoplay: true,
-          pagination: {
-            el: '.swiper-pagination'
-          }
-          // Some Swiper option/callback...
+      swiperOptions: {
+    　　observer:true,//修改swiper自己或子元素时，自动初始化swiper 
+    　　observeParents:true,//修改swiper的父元素时，自动初始化swiper 
+        loop:true,
+        autoplay: true,
+        pagination: {
+          el: '.swiper-pagination'
         }
+        // Some Swiper option/callback...
+      }
     }
+  },
+  created(){
+    
+  },
+  mounted () {
+    var that = this;
+    getHomeMultation().then(res => {
+      console.log(res);
+      this.$nextTick(() => {
+        that.swipers = res;
+      })
+      
+    }).catch(err => {
+      console.log(err)
+    })
+      //this.swiper.slideTo(3, 1000, true)
+  },
+  computed: {
+
   }
 }
 </script>
